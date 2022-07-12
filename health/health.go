f3e95e5589fb7f8e5b1ec4f7e36f7c070283cc42
Cloud User <monika.kontna@nokia.com>
2022-07-12 11:46:42 +0000

    style: comments for exported variables

--- health/health.go
+++ health/health.go
@@ -10,6 +10,7 @@ var Live = kubeprobes.NewStatefulProbe()
 //Ready represents a kubernetees probe of lievness probe.
 var Ready = kubeprobes.NewStatefulProbe()
 
+//Kp represents a kubernetees probe
 var Kp = kubeprobes.New(
 	kubeprobes.WithLivenessProbes(Live.GetProbeFunction()),
 	kubeprobes.WithReadinessProbes(Ready.GetProbeFunction()),
